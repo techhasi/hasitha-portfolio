f8d636351612460c24b3009ed191c80ad9203a16
Hasitha Wimalasooriya <57836866+techhasi@users.noreply.github.com>
2023-07-05 18:49:08 +0530

    errors fixed

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
-  return <Particles params={particlesConfig} />
 }
 
 export default MyApp
